f7a949477b1bce6eacd9661808e76e408a745d08
karthikpenumetch <63477968+karthikpenumetch@users.noreply.github.com>
2020-04-13 22:07:39 -0400

    finished java project

--- karthik.java
+++ karthik.java
@@ -2,7 +2,32 @@ public class karthik
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
-		
+		int array[] = new int[5];
+        	for(int i = 0; i < array.length; i++)
+        	{
+            		array[i] = (int)(Math.random()*5)+1;
+            		System.out.print(array[i] + " ");
+        	}
+        	System.out.println();
+        	int sum = 0;
+        	int arr[] = new int[5];
+        	for(int j = 0; j < arr.length; j++)
+        	{
+            		if(j > 1)
+            		{
+                		sum = array[j-2] + array[j-1];
+                		array[j] = sum;
+            		}
+            		else
+            		{
+                		arr[j] = array[j];
+            		}
+            		if(j >= 2)
+            		{
+                		arr[j] = sum;
+            		}
+            		System.out.print(arr[j] + " ");
+        	}
+        	System.out.println();	
 	}
 }
